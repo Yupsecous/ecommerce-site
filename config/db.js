0dbd63c1bfa38d9492f6a72afeb2a6e95f914260
Yupsecous <zxweb8895@gmail.com>
2023-10-16 01:55:20 +0600

    Add userModel schema

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URL)
-        console.log(`connected to MongoDB Database ${conn.connection.host}`)
+        console.log(`connected to MongoDB Database ${conn.connection.host}`.bgMagenta.white)
     } catch (error) {
         console.log(`Error in Mongodb ${error}`.bgRed.white)
     }
