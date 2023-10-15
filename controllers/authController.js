0b1cdefe4cfdde24a4448cfefd839b6cb62c3c0e
Yupsecous <zxweb8895@gmail.com>
2023-10-16 02:19:43 +0600

    Check existing user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,42 @@
-export const registerController = () => {
+import userModel from '../models/userModel'
 
+export const registerController = async (req, res) => {
+    try {
+        const { name, email, password, phone, address } = req.body;
+        //validations
+        if(!name) {
+            return res.send({ errpr: 'Name is Required'})
+        }
+        if(!email) {
+            return res.send({ errpr: 'Email is Required'})
+        }
+        if(!password) {
+            return res.send({ errpr: 'Password is Required'})
+        }
+        if(!phone) {
+            return res.send({ errpr: 'Phonenumber is Required'})
+        }
+        if(!address) {
+            return res.send({ errpr: 'Address is Required'})
+        }
+
+        //check user
+        const existingUser = await userModel.findOne({email});
+        if(existingUser) {
+            return res.status(200).send({
+                success: true,
+                message: 'Already Registered. Please Login'
+            })
+        }
+
+        //register user
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            success: false,
+            message: 'Error in Registration',
+            error
+        })
+    }
 }
