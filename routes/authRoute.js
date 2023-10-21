db5b5de4ac4994282671328e4aafda9e6b83164e
Yupsecous <alrexandomark@gmail.com>
2023-10-21 13:14:10 +0600

    Add login controller

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {registerController} from  '../controllers/authController.js';
+import { registerController, loginController } from  '../controllers/authController.js';
 
 //router object
 const router = express.Router()
@@ -7,5 +7,8 @@ const router = express.Router()
 //routing
 //Register || method post
 router.post('/register', registerController)
+
+//Login || method post
+router.post('/login', loginController)
 
 export default router
