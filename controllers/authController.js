60cef43f4623fbecd7f57eb7f7adfe5753cbc0a6
Yupsecous <zxweb8895@gmail.com>
2023-10-21 13:14:10 +0600

    Add login controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,9 @@
 import { trusted } from 'mongoose';
 import {hashPassword, comparePassword} from '../helpers/authHelper.js';
 import userModel from '../models/userModel.js'
+import jsonwebtoken from 'jsonwebtoken';
+import { compare } from 'bcrypt';
+import { restart } from 'nodemon';
 
 export const registerController = async (req, res) => {
     try {
@@ -34,7 +37,7 @@ export const registerController = async (req, res) => {
         //register user
         const hashedPassword = await hashPassword(password);
         //save
-        const user = new userModel({name, email, phone, address, password: hashedPassword}).save();
+        const user = await new userModel({name, email, phone, address, password: hashedPassword}).save();
         res.status(201).send({
             success: true,
             message: 'User Registered Successfully',
@@ -45,6 +48,57 @@ export const registerController = async (req, res) => {
         res.status(500).send({
             success: false,
             message: 'Error in Registration',
+            error
+        })
+    }
+}
+
+//POST LOGIN
+export const loginController = async (req,registerController) => {
+    try {
+        const { email, password } = req.body
+        //validation
+        if(!email || !password) {
+            return res.status(404).send({
+                success: false,
+                message: 'Invalid email or password'
+            })
+        }
+        //check user
+        const user = await userModel.findOne({email})
+        if(!user) {
+            return res.status(404).send({
+                success: false,
+                message: "Email is not registered"
+            })
+        }
+        const match = await comparePassword(password, user.password)
+        if(!match) {
+            return res.status(200).send({
+                success: false,
+                message: "Invalid Password"
+            })
+        }
+        //token
+        const token = await JWT.sign({_id: user._id}, process.env.JWT_SECRET, {
+            expiresIn: "7d"
+        })
+        res.status(200).send({
+            success: true,
+            message: "Logged in successfully",
+            user: {
+                name: user.name,
+                email: user.email,
+                phone: user.phone,
+                address: user.address
+            },
+            token
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            success: false,
+            message: "Error in login",
             error
         })
     }
