be35e81de32ec07702a00cdc62c93760f2a799ef
Yupsecous <zxweb8895@gmail.com>
2023-10-23 00:34:46 +0600

    Design footer

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <div className='bg-dark text-light p-3'>
+    <div className='bg-dark text-light p-3 footer'>
         <h4 className='text-center'>All Right Reserved to &copy; AliReza 2023</h4>
+        <p className='text-center mt-3'>
+          <Link to='/about'>About</Link>
+          |
+          <Link to='/cpmtact'>Contact</Link>
+          |
+          <Link to='/policy'>Priacy Policy</Link>
+        </p>
     </div>
   )
 }
