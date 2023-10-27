3d4371f36ca7a6aff6dcadcafc8b6484fef35888
Yupsecous <alrexandomark@gmail.com>
2023-10-27 14:05:00 +0600

    Add register form

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,14 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import Policy from './pages/Policy';
 import PagenotFound from './pages/PagenotFound';
+import Register from './pages/Auth/Register';
 
 function App() {
   return (
     <React.Fragment>
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='/register' element={<Register />} />
         <Route path= '/about' element={<About/>} />
         <Route path='/contact' element={<Contact/>} />
         <Route path='/policy' element={<Policy/>} />
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -3,7 +3,7 @@ import Layout from '../components/layout/Layout';
 
 const HomePage = () => {
   return (
-    <Layout>
+    <Layout title={'Best offers'}>
         <h1>Homepage</h1>
     </Layout>
   )
