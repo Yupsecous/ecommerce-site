c5906501efbd93f4d5adddfbd60dacf3d48370fc
Yupsecous <alrexandomark@gmail.com>
2023-10-29 11:13:12 +0600

    Fix the bug of fogot-password

--- client/src/pages/Auth/ForgotPassword.js
+++ client/src/pages/Auth/ForgotPassword.js
@@ -16,7 +16,7 @@ const ForgotPasssword = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post(`${process.env.REACT_APP_API}//api/v1/auth/forgot-password`, {
+      const res = await axios.post("http://localhost:8080/api/v1/auth/forgot-password", {
         email,
         newPassword,
         answer,
@@ -30,7 +30,7 @@ const ForgotPasssword = () => {
       }
     } catch (error) {
       console.log(error);
-      toast.error("Something went wrong");
+      toast.error("Something went wrong. Cannot send req");
     }
   };
   return (
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -16,7 +16,7 @@ router.post('/register', registerController)
 router.post('/login', loginController)
 
 //Forgot Password || POST
-router.post('/forget-password', forgotPasswordController)
+router.post('/forgot-password', forgotPasswordController)
 
 //test routes
 router.get('/test', requireSignIn, isAdmin, testController)
