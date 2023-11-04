fbbbe38bce9006374c2daadb78821fd75bf23e88
Yupsecous <alrexandomark@gmail.com>
2023-11-04 14:19:41 +0600

    Design the manage table

--- client/src/pages/admin/CreateCategory.js
+++ client/src/pages/admin/CreateCategory.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from '../../components/layout/Layout'
 import AdminMenu from '../../components/layout/AdminMenu'
+import toast from 'react-hot-toast'
+import axios from 'axios'
 
 const CreateCategory = () => {
+    const [categories, setCategories] = useState([])
+
+    //get all cat
+    const getAllCategory = async () => {
+        try {
+            const {data} = axios.get('/api/v1/category/get-category')
+            if (data.success) {
+                setCategories(data);
+            }
+        } catch(error) {
+            console.log(error)
+            toast.error('something went wrong in getting category')
+        }
+    };
+    useEffect(() => {
+        getAllCategory
+    }, [])
   return (
     <Layout title={'Admin- Create Category'}>
         <div className='container-fluid m-3 p-3'>
@@ -11,7 +30,38 @@ const CreateCategory = () => {
                     <AdminMenu />
                 </div>
                 <div className='col-md-9'>
-                    <h1>CreateCategory</h1>
+                    <h1>Manage Category</h1>
+                    <div>
+                        <table className="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">First</th>
+                                    <th scope="col">Last</th>
+                                    <th scope="col">Handle</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">2</th>
+                                    <td>Jacob</td>
+                                    <td>Thornton</td>
+                                    <td>@fat</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">3</th>
+                                    <td colSpan={2}>Larry the Bird</td>
+                                    <td>@twitter</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
