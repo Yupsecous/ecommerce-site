4d180fac3d302be1acfd2b7fbf66e0a5f96bd4f2
Yupsecous <zxweb8895@gmail.com>
2023-11-04 10:45:23 +0600

    Add product model. Add product routes

---
+++ controllers/productController.js
@@ -0,0 +1,13 @@
+export const createProductController = async (req, res) => {
+    try {
+
+    } catch (error) 
+    {
+        console.log(error)
+        res.status(500).send({
+            success: false,
+            error,
+            message: 'Error in creating product'
+        })
+    }
+}
