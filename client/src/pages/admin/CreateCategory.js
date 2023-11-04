e63a558e49069e8afd54ca7cf3f739a951bdced1
Yupsecous <zxweb8895@gmail.com>
2023-11-04 22:28:37 +0600

    NAdd new category attributes

--- client/src/pages/admin/CreateCategory.js
+++ client/src/pages/admin/CreateCategory.js
@@ -3,9 +3,27 @@ import Layout from '../../components/layout/Layout'
 import AdminMenu from '../../components/layout/AdminMenu'
 import toast from 'react-hot-toast'
 import axios from 'axios'
+import CategoryForm from '../../components/Form/CategoryForm'
 
 const CreateCategory = () => {
     const [categories, setCategories] = useState([])
+    const [name, setName] = useState("")
+    
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        try{
+            const {data} = await axios.post(`${process.env.REACT_APP_API}/api/v1/category/create-category`, {name})
+            if(data.success){
+                toast.success(`${data.name} is created`);
+                getAllCategory();
+            } else {
+                toast.error(data.message)
+            }
+        } catch (error){
+            console.log(error)
+            toast.error('Something went wrong in input form')
+        }
+    }
 
     //get all cat
     const getAllCategory = async () => {
@@ -32,7 +50,14 @@ const CreateCategory = () => {
                 </div>
                 <div className='col-md-9'>
                     <h1>Manage Category</h1>
-                    <div>
+                    <div className='p-3 w-50'>
+                        <CategoryForm 
+                            handleSubmit={handleSubmit} 
+                            value={name} 
+                            setValue={setName}
+                        />
+                    </div>
+                    <div className='w-75'>
                         <table className="table">
                             <thead>
                                 <tr>
@@ -41,11 +66,16 @@ const CreateCategory = () => {
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    {categories.map(c => (
-                                        <td key={c._id}>{c.name}</td>
-                                    ))}
-                                </tr>
+                                {categories?.map((c) => (
+                                    <React.Fragment>
+                                        <tr>
+                                            <td key={c._id}>{c.name}</td>
+                                            <td>
+                                                <button className='btn btn-primary'>Edit</button>
+                                            </td>
+                                        </tr>
+                                    </React.Fragment>
+                                ))}
                             </tbody>
                         </table>
                     </div>
