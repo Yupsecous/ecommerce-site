9bf87c1511750b1597a85e14c4969a57635dc8cc
Yupsecous <zxweb8895@gmail.com>
2023-11-06 02:22:28 +0600

    Add reset button for filters

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -53,7 +53,7 @@ useEffect(() => {
     if(!checked.length || !radio.length) {
       getAllProducts();
     }
-  }, [])
+  }, [checked.length, radio.length])
 
   useEffect(() => {
     if(checked.length || radio.length) filterProduct();
@@ -87,6 +87,14 @@ useEffect(() => {
             
             <h4 className='text-center'>Filter By Price</h4>
             <div className='d-flex flex-column'>
+              <button 
+                className='btn btn-danger'
+                onClick={() => window.location.reload()}
+              >
+                Reset filters
+              </button>
+            </div>
+            <div className='d-flex flex-column'>
               <Radio.Group onChange={(e) => setRadio(e.target.value)}>
                 {Prices?.map((p) => (
                   <div key={p._id}>
@@ -110,7 +118,8 @@ useEffect(() => {
                     />
                     <div className="card-body">
                       <h5 className="card-title">{p.name}</h5>
-                      <p className="card-text">{p.description}</p>
+                      <p className="card-text">{p.description.substring(30)}</p>
+                      <p className="card-text">$ {p.price}</p>
                       <button className='btn btn-primary ms-1'>More Details</button>
                       <button className='btn btn-secondary ms-1'>Add to cart</button>
                     </div>
