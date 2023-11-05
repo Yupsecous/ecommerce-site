7133194fd16c58e21060fbec743380ea01fefd06
Yupsecous <alrexandomark@gmail.com>
2023-11-05 14:38:07 +0600

    Fix the bugs. Still one in update selected

--- client/src/App.js
+++ client/src/App.js
@@ -31,13 +31,14 @@ function App() {
           <Route path='user/profile' element={<Profile />} />
           <Route path='user/orders' element={<Orders />} />
         </Route>
-        <Route path='/dashboard' element={<AdminRoute/>}>
-          <Route path='admin' element={<AdminDashboard />} />
-          <Route path='admin/create-category' element={<CreateCategory />} />
-          <Route path='admin/create-product' element={<CreateProduct />} />
-          <Route path='admin/product/:slug' element={<UpdateProduct />} />
-          <Route path='admin/users' element={<Users />} />
-          <Route path='admin/products' element={<Products />}
+        <Route path="/dashboard" element={<AdminRoute />}>
+          <Route path="admin" element={<AdminDashboard />} />
+          <Route path="admin/create-category" element={<CreateCategory />} />
+          <Route path="admin/create-product" element={<CreateProduct />} />
+          <Route path="admin/product/:slug" element={<UpdateProduct />} />
+          <Route path="admin/products" element={<Products />} />
+          <Route path="admin/users" element={<Users />} />
+          <Route path="admin/orders" element={<Orders />} />
         </Route>
         <Route path='/register' element={<Register />} />
         <Route path='/forgot-password' element={<ForgotPasssword />} />
