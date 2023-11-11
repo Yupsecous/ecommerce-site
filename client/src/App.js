56738f603558f846655dfa0675b0955162343ca8
Yupsecous <zxweb8895@gmail.com>
2023-11-12 00:47:31 +0600

    Add Search Item

--- client/src/App.js
+++ client/src/App.js
@@ -19,13 +19,15 @@ import Profile from './pages/user/Profile';
 import Orders from './pages/user/Orders';
 import Products from './pages/admin/Products';
 import UpdateProduct from './pages/admin/UpdateProduct';
+import Search from './pages/Search';
 
 
 function App() {
   return (
     <React.Fragment>
       <Routes>
-        <Route path='/' element={<HomePage />} />
+        <Route path='/' element={<HomePage />} /> 
+        <Route path='/search' element={<Search />} />        
         <Route path='/dashboard' element={<PrivateRoute/>}>
           <Route path='user' element={<Dashboard />} />
           <Route path='user/profile' element={<Profile />} />
