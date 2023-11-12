2ad57ad2642ff2bb0280537ef682f5ebc521a95c
Yupsecous <alrexandomark@gmail.com>
2023-11-12 21:18:43 +0600

    Update profile

--- client/src/pages/user/Profile.js
+++ client/src/pages/user/Profile.js
@@ -1,124 +1,130 @@
-import React, { useState } from 'react'
-import Layout from '../../components/layout/Layout'
-import UserMenu from '../../components/layout/UserMenu'
-import { useAuth } from '../../context/auth'
-import toast from 'react-hot-toast'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import UserMenu from "../../components/Layout/UserMenu";
 
+import { useAuth } from "../../context/auth";
+import toast from "react-hot-toast";
+import axios from "axios";
+import Layout from "antd/es/layout/layout";
 const Profile = () => {
-    const [auth, setAuth] = useAuth()
+    const reactApi = process.env.REACT_APP_API
+  //context
+  const [auth, setAuth] = useAuth();
+  //state
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
 
-    const [name, setName] = useState("")
-    const [email, setEmail] =useState("")
-    const [password, setPassword] = useState("")
-    const [phone, setPhone] = useState("")
-    const [address, setAddress] = useState("")
+  //get user data
+  useEffect(() => {
+    const { email, name, phone, address } = auth?.user;
+    setName(name);
+    setPhone(phone);
+    setEmail(email);
+    setAddress(address);
+  }, [auth?.user]);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const res = await axios.post(`${process.env.REACT_APP_API}/api/v1/auth/register`, 
-            {name, email, password, phone, address});
-            if( res && res.data.success) {
-                toast.success(res.data && res.data.message)
-            } else {
-                toast.error(res.data.message)
-            }
-
-        } catch (error) {
-           console.log(error)
-           toast.error('Something went wrong') 
-        }
+  // form function
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.put(`${reactApi}/api/v1/auth/profile`, {
+        name,
+        email,
+        password,
+        phone,
+        address,
+      });
+      if (data?.error) {
+        toast.error(data?.error);
+      } else {
+        setAuth({ ...auth, user: data?.updatedUser });
+        let ls = localStorage.getItem("auth");
+        ls = JSON.parse(ls);
+        ls.user = data.updatedUser;
+        localStorage.setItem("auth", JSON.stringify(ls));
+        toast.success("Profile Updated Successfully");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
     }
+  };
   return (
-    <Layout title={'User-Profile'}>
-        <div className='container-fluid m-3 p-3'>
-            <div className='row'>
-                <div className='col-md-3'>
-                    <UserMenu />
+    <Layout title={"Your Profile"}>
+      <div className="container-fluid m-3 p-3 dashboard">
+        <div className="row">
+          <div className="col-md-3">
+            <UserMenu />
+          </div>
+          <div className="col-md-8">
+            <div className="form-container" style={{ marginTop: "-40px" }}>
+              <form onSubmit={handleSubmit}>
+                <h4 className="title">USER PROFILE</h4>
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className="form-control"
+                    id="exampleInputEmail1"
+                    placeholder="Enter Your Name"
+                    autoFocus
+                  />
                 </div>
-                <div className='col-md-9'>
-                    
-                    <form onSubmit={handleSubmit}>
-                        <h4 className='title'>User Profile</h4>
-                        <div className="mb-3">
-                            <input 
-                                type="text" 
-                                value={name}
-                                onChange={(e) => setName(e.target.value)}
-                                className="form-control" 
-                                id="name" 
-                                placeholder='Enter your name' 
-                                required
-                            />
-                        </div>
-                        <div className="mb-3">
-                            <input 
-                                type="email" 
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                className="form-control" 
-                                id="email" 
-                                placeholder='Enter your E-mail' 
-                                required
-                            />
-                        </div>
-                        <div className="mb-3">
-                            <input 
-                                type="password" 
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                className="form-control" 
-                                id="password" 
-                                placeholder='Password' 
-                                required 
-                            />
-                        </div>
-                        <div className="mb-3">
-                            <input 
-                                type="text" 
-                                value={phone}
-                                onChange={(e) => setPhone(e.target.value)}
-                                className="form-control" 
-                                id="phone" 
-                                placeholder='Phone No.'  
-                                required
-                            />
-                        </div>
-                        <div className="mb-3">
-                            <input 
-                                type="text" 
-                                value={address}
-                                onChange={(e) => setAddress(e.target.value)}
-                                className="form-control" 
-                                id="address" 
-                                placeholder='Address' 
-                                required
-                            />
-                        </div>
-                        <div className="mb-3">
-                            <input 
-                                type="text" 
-                                value={answer}
-                                onChange={(e) => setAnswer(e.target.value)}
-                                className="form-control" 
-                                id="address" 
-                                placeholder='Your pet name?' 
-                                required
-                            />
-                        </div>
-                        <button 
-                            type="submit" 
-                            className="btn btn-primary"
-                        >
-                            Submit
-                        </button>
-                    </form>
+                <div className="mb-3">
+                  <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="form-control"
+                    id="exampleInputEmail1"
+                    placeholder="Enter Your Email "
+                    disabled
+                  />
                 </div>
-            </div>
-        </div>
-    </Layout>
-  )
-}
+                <div className="mb-3">
+                  <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="form-control"
+                    id="exampleInputPassword1"
+                    placeholder="Enter Your Password"
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                    className="form-control"
+                    id="exampleInputEmail1"
+                    placeholder="Enter Your Phone"
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    value={address}
+                    onChange={(e) => setAddress(e.target.value)}
+                    className="form-control"
+                    id="exampleInputEmail1"
+                    placeholder="Enter Your Address"
+                  />
+                </div>
 
-export default Profile
+                <button type="submit" className="btn btn-primary">
+                  UPDATE
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Profile;
