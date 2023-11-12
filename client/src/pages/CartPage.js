e23a567a36b58de2b15a69802cd71aeb3ab9c9ae
Yupsecous <zxweb8895@gmail.com>
2023-11-12 10:01:45 +0600

    Use useCart context to the homepage

---
+++ client/src/pages/CartPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Layout from '../components/layout/Layout'
+
+const CartPage = () => {
+  return (
+    <Layout>
+        <div className='container'>
+            <div className='row'>
+                <div className='col-md-9'>
+                </div>
+            </div>
+        </div>
+    </Layout>
+  )
+}
+
+export default CartPage
