23531f2a31f764cd089ee0355cad3a41c6227655
Yupsecous <zxweb8895@gmail.com>
2023-11-13 10:43:44 +0600

    Add order controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ import {hashPassword, comparePassword} from '../helpers/authHelper.js';
 import userModel from '../models/userModel.js'
 import JWT from 'jsonwebtoken';
 import { compare } from 'bcrypt';
+import orderModel from '../models/orderModel.js';
 
 export const registerController = async (req, res) => {
     try {
@@ -180,6 +181,25 @@ export const updateProfileController = async (req, res) => {
         res.status(500).send({
             success: false,
             message: "something went wrong",
+            error
+        })
+    }
+}
+
+// orders
+
+export const getOrdersController = async (req, res) => {
+    try {
+        const orders = await orderModel
+        .find({buyer: req.user._id})
+        .populate("products", "-photo")
+        .populate("buyer","name");
+        res.json(orders)
+    } catch(error) {
+        console.log(error)
+        res.status(500).send({
+            success: false,
+            message: "Error while getting orders",
             error
         })
     }
